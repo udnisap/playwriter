45ca51bc354e66ac1c08ae9d2b025787b3bebb21
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-12-30 17:39:52 +0100

    Update _index.tsx

--- website/src/routes/_index.tsx
+++ website/src/routes/_index.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'react-router';
 
 export const loader = () => {
-  return redirect('https://github.com/remorses/playwriter');
+  throw redirect('https://github.com/remorses/playwriter');
 };
 
 export default function Index() {
